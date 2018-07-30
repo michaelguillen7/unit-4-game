cb68d68ec6da9b7bd2cb774ffb4f8462d81d5b1a
Michael Guillen <michaelguillen@Michaels-MBP.attlocal.net>
2018-07-30 00:53:08 -0500

    js update 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,3 +14,29 @@ console.log("Number to Reach: " + randomNumberGenerator);
 $('#WinsNumber').html("Wins: " + wins);
 $('#LosesNumber').html("Losses: " + losses);
 $('#randomNumber').html("Number to Reach: " + randomNumberGenerator);
+
+$("#one").attr("data-crystalvalue", crystalArry[0]);
+$("#two").attr("data-crystalvalue", crystalArry[1]);
+$("#three").attr("data-crystalvalue", crystalArry[2]);
+$("#four").attr("data-crystalvalue", crystalArry[3]);
+
+$("#one, #two, #three, #four").on("click", function(){
+var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    Total += crystalValue;
+    $('#finalTotal').html("Your Total Score Is: " + Total);
+    console.log(crystalValue)
+    
+    if (Total === randomNumberGenerator) {
+        alert("You win!");
+        wins++;
+        $('#WinsNumber').html("Wins: " + wins);
+        reset()
+    }
+    else if (Total > randomNumberGenerator) {
+        alert("You lose!");
+        losses++;
+        $('#LosesNumber').html("Losses: " + losses);
+        reset()
+    }
+})
